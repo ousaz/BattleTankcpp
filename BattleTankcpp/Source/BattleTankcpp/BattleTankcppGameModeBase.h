88128b1551b3b1f2652638d2ba74065714944631
ousaz <ousaz123@gmail.com>
2018-11-30 02:17:47 +0200

    creating

---
+++ BattleTankcpp/Source/BattleTankcpp/BattleTankcppGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankcppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANKCPP_API ABattleTankcppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
